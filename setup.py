270749682370269b61fe4d1f9a072f09379efd68
Udaykiran87 <uday.patnaik@gmail.com>
2022-07-14 21:44:32 +0900

    updated initial installation setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "modular_code_repository"
+REPO_NAME = "OD_DeepFashion2"
 AUTHOR_USER_NAME = "Udaykiran87"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = []
@@ -14,7 +14,7 @@ setup(
     name=SRC_REPO,
     version="0.0.1",
     author=AUTHOR_USER_NAME,
-    description="Template repository for modular coding",
+    description="Object detection using detectron2",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
